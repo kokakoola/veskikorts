67eaf92a691bb2b2ec7771f6b09826db02dff21e
Mare Hunt <kuremant@gmail.com>
2011-05-02 14:47:37 +0300

    stable index page

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#flag div').hide();
+    $('#flag div').first().show();
+    $('#flag ul li').first().addClass('active');
+   
+    $('#flag ul li a').click(function(){
+        $('#flag ul li').removeClass('active');
+        $(this).parent().addClass('active');
+        
+        var currentDay = $(this).attr('href');
+        $('#flag div').hide();
+        $(currentDay).fadeIn();
+        return false;
+    });
+    
+    $('ul#slides').cycle( {
+        fx: 'fade',
+        pause: 2
+    });
+    
+});
+
+
